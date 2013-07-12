44f9539f2dfb12ced0fb629982d38abe9ab530f7
saurabh kulkarni <skulk1001@gmail.com>
2013-07-12 05:45:42 +0000

    updatd webjs

--- web.js
+++ web.js
@@ -2,21 +2,14 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-fs.readFileSync('/index.html', function(err,data){
-    if (err) throw err;
-    console.log(data);
-});
-
-//var output = data.toString('utf8',0,data.length);
-
-//console.log(fileContents);
-console.log(output);
-
-app.get('/', function(request, response) {
-
-filecontents = fs.readFileSync('index.html');
-output = filecontents.toString('utf8',0);
-response.send(output); 
+//fs.readFileSync('/index.html', function(err,data){
+//    if (err) throw err;
+//    console.log(data);
+//});
+ app.get('/', function(request, response) {
+    var filecontents = fs.readFileSync('index.html');
+    var output = filecontents.toString('utf8',0);
+    response.send(output);
 });
 
 var port = process.env.PORT || 5000;
