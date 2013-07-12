cba312084fada5da36eb43b2c8f08537b3387a78
saurabh kulkarni <skulk1001@gmail.com>
2013-07-12 04:09:26 +0000

    linked to data from index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+    var fileContents = fs.readFileSync('index.html');
+    var output = fileContents.toString(0,fileContents.length);
+
+response.send(output);
 });
 
 var port = process.env.PORT || 5000;
