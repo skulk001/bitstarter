3a0a5b4cc9585dbdd27b8c57fc4be12d56e0a1e4
saurabh kulkarni <skulk1001@gmail.com>
2013-07-12 04:51:15 +0000

    linked to data from index.html

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 var fileContents = fs.readFileSync('index.html');
 var output = fileContents.toString('utf8',0,fileContents.length);
+console.log(fileContents);
+console.log(output);
 
 app.get('/', function(request, response,output) {
        response.send(output);
